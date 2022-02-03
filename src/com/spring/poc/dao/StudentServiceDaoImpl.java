c75e2dd48fbd1536814855a336f691a5a0e541d8
Subhranil Sengupta <subhranilofficial10@gmail.com>
2022-02-03 00:20:23 -0600

    Jboss datasources configured

--- src/com/spring/poc/dao/StudentServiceDaoImpl.java
+++ src/com/spring/poc/dao/StudentServiceDaoImpl.java
@@ -1,8 +1,21 @@
 package com.spring.poc.dao;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.sql.DataSource;
+
 import com.spring.poc.model.Student;
 
 public class StudentServiceDaoImpl implements StudentServiceDao {
+
+	Connection conn = null;
+	Statement stm = null;
+	ResultSet rs = null;	
+	
+	private DataSource datasource;
 
 	@Override
 	public Student addStudent() {
@@ -10,7 +23,19 @@ public class StudentServiceDaoImpl implements StudentServiceDao {
 		Student student = new Student();
 		student.setId(34);
 		student.setName("Subhranil");
-		student.setScore(56.3);
+		student.setScore(56.3);	
+		
+		try {
+			conn = datasource.getConnection();
+			stm = conn.createStatement();
+			rs = stm.executeQuery("select * from USR");
+			
+			System.out.println("Printed : " + rs);
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		return student;
 		
